3e3ff90e29f2a80ae32a185dae95a9304fe88407
mukhammad ginanjar azie <azie@aegis.co.id>
2016-03-15 06:42:48 +0700

    route to dashboard if login succeed

--- STANDARD/assets/js/controllers/loginCtrl.js
+++ STANDARD/assets/js/controllers/loginCtrl.js
@@ -1,5 +1,5 @@
-app.controller('LoginCtrl', ["$scope", "$http", "SweetAlert", "$cookies",
-               function ($scope, $http, SweetAlert, $cookies) {
+app.controller('LoginCtrl', ["$scope", "$http", "SweetAlert", "$cookies", "$location",
+               function ($scope, $http, SweetAlert, $cookies, $location) {
 
     // $http.defaults.headers.post['Content-Type'] = 'application/json; charset=UTF-8';
 
@@ -24,14 +24,28 @@ app.controller('LoginCtrl', ["$scope", "$http", "SweetAlert", "$cookies",
                 data: $.param({
                     username: $scope.username,
                     password: CryptoJS.SHA1($scope.password).toString(),
-                    app_key: '415d6190a6e11f06db5a970d04f6a2ea26e1cdb9',
                 }),
-                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'app_key': '415d6190a6e11f06db5a970d04f6a2ea26e1cdb9'
+                }
             })
             .then(function(response) {
                 $cookies.procurementSession = response.data.objects[0].access_token;
+
+                $location.path('app/dashboard');
             }, function(err) {
-                console.log(err.data);
+                SweetAlert.swal({
+                    title: "Perhatian",
+                    text: "Username dan Password tidak cocok",
+                    type: "warning",
+                    showCancelButton: false,
+                    confirmButtonColor: "#1FBBA6",
+                    confirmButtonText: "OK",
+                    closeOnConfirm: true}, 
+                function(){ 
+                        
+                });
             });
         }
     }
